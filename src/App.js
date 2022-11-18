73f65c08262c99574f21cee3124dd0a939b85a0d
thuotracy <tracyjacobs775@gmail.com>
2022-11-18 08:52:07 +0300

    made changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/sidebar/Sidebar";
 
 
 function App() {
   return (
     <div>
         <Navbar/>
+        <div className='container'>
+          <Sidebar/>
+          other pages
+        </div>
     </div>
   );
 }
