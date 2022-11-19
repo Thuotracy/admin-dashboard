0a3f55a0e570bb91a44a5b7a88730801e13fa9b3
thuotracy <tracyjacobs775@gmail.com>
2022-11-19 23:34:48 +0300

    changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <div className='navbar'>
         <div className="navbarwrapper">
             <div className="topLeft">
-                <span className='logo'>Admin</span>
+                <span className='logo'>Admin Dashboard </span>
             </div>
             <div className="topRight">
                 <div className="navbarIconContainer">
