b14bc562bb646736f11cc6d1fbe0a403b8ec4458
thuotracy <tracyjacobs775@gmail.com>
2022-11-21 15:25:54 +0300

    created app.css fiel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Navbar from "./components/navbar/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
+import './app.css'
 
 
 function App() {
   return (
     <div>
-        <Navbar/>
+        {/* <Navbar/> */}
         <div className='container'>
           <Sidebar/>
-          other pages
+          <div>other pages</div>
         </div>
     </div>
   );
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,7 +3,7 @@ import './sidebar.css';
 export default function Sidebar() {
   return (
     <div className='sidebar'>
-        
+        sidebar
     </div>
   )
 }
