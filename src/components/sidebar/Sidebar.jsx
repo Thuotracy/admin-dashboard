3b0c0b4b4c92a97cd3d38c461a5442c7319a0e6c
thuotracy <tracyjacobs775@gmail.com>
2022-11-25 07:27:57 +0300

    created the main menu

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,28 +1,103 @@
-import { LineStyle, Timeline, TrendingUp } from '@material-ui/icons';
-import './sidebar.css';
+import "./sidebar.css";
+import {
+  LineStyle,
+  Timeline,
+  TrendingUp,
+  PermIdentity,
+  Storefront,
+  AttachMoney,
+  BarChart,
+  MailOutline,
+  DynamicFeed,
+  ChatBubbleOutline,
+  WorkOutline,
+  Report,
+} from "@material-ui/icons";
+
 
 export default function Sidebar() {
   return (
-    <div className='sidebar'>
-        <div className="sidebarWrapper">
-          <div className="sidebarMenu">
-            <h3 className='sidebarTitle'>DashBoard</h3>
-              <ul className='sidebarList'>               
-                <li className='sidebarListItem active'>
-                  <LineStyle className='sidebarIcon'/>
-                    Home
-                </li>
-                <li className='sidebarListItem'>
-                  <Timeline className='sidebarIcon'/>
-                    Analytics                  
-                </li>
-                <li className='sidebarListItem'>
-                  <TrendingUp className='sidebarIcon'/>
-                    Sales
-                </li>
-              </ul>
-          </div>
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+           
+            <li className="sidebarListItem active">
+              <LineStyle className="sidebarIcon" />
+              Home
+            </li>
+            
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUp className="sidebarIcon" />
+              Sales
+            </li>
+          </ul>
         </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Main  Menu</h3>
+          <ul className="sidebarList">
+            
+              <li className="sidebarListItem">
+                <PermIdentity className="sidebarIcon" />
+                Users
+              </li>
+            
+           
+              <li className="sidebarListItem">
+                <Storefront className="sidebarIcon" />
+                Products
+              </li>
+          
+            <li className="sidebarListItem">
+              <AttachMoney className="sidebarIcon" />
+              Transactions
+            </li>
+            <li className="sidebarListItem">
+              <BarChart className="sidebarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <MailOutline className="sidebarIcon" />
+              Mail
+            </li>
+            <li className="sidebarListItem">
+              <DynamicFeed className="sidebarIcon" />
+              Feedback
+            </li>
+            <li className="sidebarListItem">
+              <ChatBubbleOutline className="sidebarIcon" />
+              Messages
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Staff</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <WorkOutline className="sidebarIcon" />
+              Manage
+            </li>
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <Report className="sidebarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
