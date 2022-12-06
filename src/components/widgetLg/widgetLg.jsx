f4f76c65fb279240588e893fff465b3cd514f7c9
thuotracy <tracyjacobs775@gmail.com>
2022-12-06 13:33:59 +0300

    created title

--- src/components/widgetLg/widgetLg.jsx
+++ src/components/widgetLg/widgetLg.jsx
@@ -2,6 +2,8 @@ import './widgetLg.css'
 
 export default function widgetLg() {
   return (
-    <div className='widgetLg'>large</div>
+    <div className='widgetLg'>
+        <h3 className='widgetLgTitle'>Recent Transactions</h3>
+    </div>
   )
 }
