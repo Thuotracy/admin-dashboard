ee428785129fe686db1d7cf39997cbaeb473c7f4
thuotracy <tracyjacobs775@gmail.com>
2022-12-06 13:40:11 +0300

    created a table

--- src/components/widgetLg/widgetLg.jsx
+++ src/components/widgetLg/widgetLg.jsx
@@ -4,6 +4,14 @@ export default function widgetLg() {
   return (
     <div className='widgetLg'>
         <h3 className='widgetLgTitle'>Recent Transactions</h3>
+        <table className='widgetLgTable'>
+            <tr className='widgetLgTr'>
+                <th className='widgetLgTh'>Customer</th>
+                <th className='widgetLgTh'>Date</th>
+                <th className='widgetLgTh'>Amount</th>
+                <th className='widgetLgTh'>Status</th>
+            </tr>
+        </table>
     </div>
   )
 }
