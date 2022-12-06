717a6af2dacaa57eafbd922111db536bd9d1b0f6
thuotracy <tracyjacobs775@gmail.com>
2022-12-06 14:17:30 +0300

    made changes

--- src/components/widgetLg/widgetLg.jsx
+++ src/components/widgetLg/widgetLg.jsx
@@ -1,6 +1,11 @@
 import './widgetLg.css'
 
 export default function widgetLg() {
+
+    const Button = ({ type }) => {
+        return <button className={"widgetLgButton " + type}>{type}</button>
+    };
+
   return (
     <div className='widgetLg'>
         <h3 className='widgetLgTitle'>Recent Transactions</h3>
@@ -10,6 +15,50 @@ export default function widgetLg() {
                 <th className='widgetLgTh'>Date</th>
                 <th className='widgetLgTh'>Amount</th>
                 <th className='widgetLgTh'>Status</th>
+            </tr>
+            <tr className='widgetLgTr'>
+                <td className='widgetLgUser'>
+                    <span className='widgetLgName'>John Doe</span>
+                </td>
+                <td className='widgetLgDate'>21 Aug 2022</td>
+                <td className='widgetLgAmount'>$83.99</td>
+                <td className='widgetLgStatus'><Button type="Approved"/></td>
+            </tr>
+
+            <tr className='widgetLgTr'>
+                <td className='widgetLgUser'>
+                    <span className='widgetLgName'>John Doe</span>
+                </td>
+                <td className='widgetLgDate'>21 Aug 2022</td>
+                <td className='widgetLgAmount'>$83.99</td>
+                <td className='widgetLgStatus'><Button type="Approved"/></td>
+            </tr>
+
+            <tr className='widgetLgTr'>
+                <td className='widgetLgUser'>
+                    <span className='widgetLgName'>John Doe</span>
+                </td>
+                <td className='widgetLgDate'>21 Aug 2022</td>
+                <td className='widgetLgAmount'>$83.99</td>
+                <td className='widgetLgStatus'><Button type="Pending"/></td>
+            </tr>
+
+            <tr className='widgetLgTr'>
+                <td className='widgetLgUser'>
+                    <span className='widgetLgName'>John Doe</span>
+                </td>
+                <td className='widgetLgDate'>21 Aug 2022</td>
+                <td className='widgetLgAmount'>$83.99</td>
+                <td className='widgetLgStatus'><Button type="Declined"/></td>
+            </tr>
+
+            <tr className='widgetLgTr'>
+                <td className='widgetLgUser'>
+                    <span className='widgetLgName'>John Doe</span>
+                </td>
+                <td className='widgetLgDate'>21 Aug 2022</td>
+                <td className='widgetLgAmount'>$83.99</td>
+                <td className='widgetLgStatus'><Button type="Pending"/></td>
             </tr>
         </table>
     </div>
