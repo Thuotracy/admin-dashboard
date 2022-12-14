89b69a5b8053a37d7be497c07fa4e03b01ef12de
thuotracy <tracyjacobs775@gmail.com>
2022-12-14 17:56:11 +0300

    added browserrouter

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import Navbar from "./components/navbar/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import './app.css'
 import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 
 function App() {
   return (
-    <div>
+    <Router>
         <Navbar/>
         <div className='container'>
           <Sidebar/>
-          <Home/>
+          <Switch>
+            <Route path="/">
+              <Home/>
+            </Route>  
+            <Route path="/users">
+              <UserList/>
+            </Route>  
+          </Switch>      
         </div>
-    </div>
+    </Router>
   );
 }
 
